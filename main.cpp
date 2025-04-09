23779192dfde28a3f677bf04d8f08160c617663a
RhysticPatrol <stevenswilson7798@gmail.com>
2025-04-08 22:42:41 -0400

    adding comments to main

--- main.cpp
+++ main.cpp
@@ -4,22 +4,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() { // creating cars for the officer to inspect
     ParkedCar toyotaCamry("XYZ123", "Toyota", "Camry", "Red");
     ParkedCar hondaAccord("ABC987", "Honda", "Accord", "Blue");
     ParkedCar fordMustang("LMN456", "Ford", "Mustang", "Black");
     ParkedCar nissanAltima("DEF789", "Nissan", "Altima", "Silver");
     ParkedCar chevyMalibu("GHI012", "Chevrolet", "Malibu", "White");
 
+    // creating meters for the cars
     ParkingMeter meter1(40, 30);
     ParkingMeter meter2(60, 70);
     ParkingMeter meter3(60, 190);
     ParkingMeter meter4(90, 80);
     ParkingMeter meter5(120, 240);
 
-    PoliceOfficer officer("John Policeman", "COP123", toyotaCamry, meter1);
+    PoliceOfficer officer("John Policeman", "COP123", toyotaCamry, meter1); // creating the officer
 
-    officer = PoliceOfficer("John Policeman", "COP123", toyotaCamry, meter1);
+    officer = PoliceOfficer("John Policeman", "COP123", toyotaCamry, meter1); // running inspections for the cars
     officer.inspectCar();
     cout << "\n";
 
@@ -35,7 +36,7 @@ int main() {
     officer.inspectCar();
     cout << "\n";
 
-    officer = PoliceOfficer("John Policeman", "COP123", chevyMalibu, meter5);
+    officer = PoliceOfficer("John Policeman", "COP123", chevyMalibu, meter5); // last car inspected
     officer.inspectCar();
     cout << "\n";
 
